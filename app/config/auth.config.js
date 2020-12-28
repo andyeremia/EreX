2bc15aa6f8a7056165cc23d368e557685d9cebe0
Andrei Eremia <andrei.eremia@aquasoft.ro>
2020-12-28 21:57:50 +0200

    BACKEND: -> user registration complete -> user authentication complete

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secret: "ereX-b@tm@n",
+};
---
+++ app/config/db.config.js
@@ -0,0 +1,11 @@
+// db_ere
+// 123456!
+// mongodb+srv://db_ere:<password>@erexdb.vmarh.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+module.exports = {
+  HOST: "localhost",
+  PORT: 27017,
+  USERNAME: "db_ere",
+  PASSWORD: "123456!",
+  BD: "erex_DB",
+};
